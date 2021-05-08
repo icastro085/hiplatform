a07cb690de89c71c890af60bd79a41974dac168b
Ivanildo de Castro <ivanildo.castro@gaivota.ai>
2021-05-08 00:07:28 -0400

    feat: Added api fake request

---
+++ src/api/service.test.ts
@@ -0,0 +1,8 @@
+import { getAll } from './server';
+
+jest.mock('../../data/items.json', () => true);
+
+test('should server return list of items', async () => {
+  const items = await getAll();
+  expect(items).toBe(true);
+});
