025d75353aeea63c241332f3de6b2dee46dc1439
Ivanildo de Castro <ivanildo.castro@gaivota.ai>
2021-05-08 15:35:56 -0400

    feat: Added db api and Provider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { GlobalStyle } from './styles/shared';
 import Router from './Router';
 import Container from './components/Container';
+import DBProvider from './components/DBProvider';
 
 export default function App() {
   return (
     <>
-      <Container>
-        <Router />
-      </Container>
+      <DBProvider>
+        <Container>
+          <Router />
+        </Container>
+      </DBProvider>
       <GlobalStyle />
     </>
   );
